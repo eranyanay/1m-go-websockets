e51d874467634eb7c3e728ffb4d8f6b171df951b
Dmytro Tananayskiy <dmitriyminer@gmail.com>
2019-03-03 00:28:31 +0200

    GOOS for the target operating system build

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 CONNECTIONS=$1
 REPLICAS=$2
 IP=$3
-go build --tags "static netgo" -o client client.go
+GOOS=linux go build --tags "static netgo" -o client client.go
 for (( c=0; c<${REPLICAS}; c++ ))
 do
     docker run -l 1m-go-websockets -v $(pwd)/client:/client -d alpine /client -conn=${CONNECTIONS} -ip=${IP}
